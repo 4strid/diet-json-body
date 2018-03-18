174ebbc7798c409216cf0e0a1b5b4cdb065eafeb
Peter Fesz-Nguyen <pfesznguyen@gmail.com>
2018-03-18 15:39:44 -0700

    sometimes there's no content-type

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = function ($) {
 	const contType = $.request.headers['content-type']
 	// trim off ;charset=UTF-8 and any other options
-	if (contType.split(';')[0].trim() === 'application/json') {
+	if (contType && contType.split(';')[0].trim() === 'application/json') {
 		// it might have already been successfully parsed by Diet's native body parser
 		if (typeof $.body === 'string') {
 			try {
